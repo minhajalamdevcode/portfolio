c117b0279a5f470a5a7464053a8fe6a5a1bcc420
minhaj alam <myminhajalam392@gmail.com>
2025-08-19 12:05:26 +0530

    add image

--- src/components/Skills & Tools/data.js
+++ src/components/Skills & Tools/data.js
@@ -1,6 +1,14 @@
 export const data = [
   {
     id: 1,
+    skillName: 'Word,Excel',
+    skillContent:
+      "Document formatting, report creation, editing & professional documentation. and Data entry, formulas, pivot tables, charts & data analysis.",
+    src: '/office.jpg',
+  
+  },
+  {
+    id: 2,
     skillName: 'HTML, CSS, SCSS',
     skillContent:
       "I use HTML to structure websites and CSS/SCSS to design responsive, elegant interfaces across all screen sizes.",
@@ -8,42 +16,42 @@ export const data = [
   
   },
   {
-    id: 2,
+    id: 3,
     skillName: 'Tailwind CSS, Bootstrap',
     skillContent:
       "I use Tailwind CSS for highly customizable, utility-first styling, and Bootstrap to rapidly develop responsive layouts with prebuilt components.",
     src: '/tailwindcssbootstrap.png',
   },
   {
-    id: 3,
+    id: 4,
     skillName: 'JavaScript, React, React Router',
     skillContent:
       "JavaScript powers dynamic web behavior. React helps build modular UI components, while React Router manages client-side navigation.",
     src: '/javascriptreactrouter.png',
   },
   {
-    id: 4,
+    id: 5,
     skillName: ' Redux Toolkit',
     skillContent:
       "I manage complex app states using Redux, and streamline logic with Redux Toolkit for scalable architecture.",
     src: '/reduxtoolkit.jpg',
   },
   {
-    id: 5,
+    id: 6,
     skillName: 'GSAP, Framer Motion',
     skillContent:
       "I use GSAP for powerful animation timelines and Framer Motion for declarative, React-native animations that enhance UX.",
     src: '/gsapframermotion.jpg',
   },
   {
-    id: 6,
+    id: 7,
     skillName: 'PHP, MySQL',
     skillContent:
       "I use Dynamic server-side scripting with PHP to build robust web applications Efficient database design and querying using MySQL for fast, secure data handling.",
     src: '/phpmysql.jpg',
   },
   {
-    id: 7,
+    id: 8,
     skillName: 'WordPress',
     skillContent:
       "Experienced in building dynamic, responsive websites using WordPress. Skilled in customizing themes, developing plugins, and optimizing performance for SEO and speed. Proficient in managing content, securing WordPress installations, and integrating third-party tools for extended functionality.",
@@ -51,7 +59,7 @@ export const data = [
   },
 
   {
-    id: 8,
+    id: 9,
     skillName: 'Git, GitHub',
     skillContent:
       "I use Git for version control and GitHub to collaborate, manage repositories, and maintain code history across teams.",
@@ -59,21 +67,21 @@ export const data = [
   },
   
   {
-    id: 9,
+    id: 10,
     skillName: 'VS Code, Cursor AI, Firebase Studio',
     skillContent:
       "My go-to code editors include VS Code and next-gen AI-powered editors like Cursor AI, Firebase Studio for writing cleaner, smarter code efficiently.",
     src: '/vscode.png',
   },
   {
-    id: 10,
+    id: 11,
     skillName: 'Firefox, Chrome',
     skillContent:
       "I test and debug my web projects across multiple browsers to ensure compatibility, performance, and accessibility.",
     src: '/chrome.jpg',
   },
   {
-    id: 11,
+    id: 12,
     skillName: 'ChatGPT',
     skillContent:
       "I use ChatGPT to brainstorm, debug, optimize code, and accelerate my learning and development process.",
